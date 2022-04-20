e75590267a6863833844c10906b8361f0e7db9d1
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-20 16:50:24 -0400

    add components into appointment comp/ add stories as well

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,32 +1,46 @@
 import React, { useState } from "react";
 import Button from "components/Button";
-import InterviewerList from "components/InterviewerListItem";
+import InterviewerList from "components/InterviewerList";
 
 
 export default function Form(props) {
+  const [student, setStudent] = useState(props.student || '');
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  function reset() {
+    setStudent('');
+    setInterviewer(null);
+  }
+
+  function cancel() {
+    reset();
+    return props.onCancel();
+  }
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off"
+         onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-            your code goes here
-          */
+            value={student}
+            onChange={(e) => setStudent(e.target.value)}
           />
         </form>
         <InterviewerList
-        /* your code goes here */
+          value={interviewer}
+          interviewers={props.interviewers}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger /*code goes here */ >Cancel</Button>
-          <Button confirm /* code goes here */>Save</Button>
+          <Button danger onClick={cancel}>Cancel</Button>
+          <Button confirm onClick={() => props.onSave(student, interviewer)}>Save</Button>
         </section>
       </section>
     </main>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
-import 'components/Appointment/styles.scss'
+import 'components/Appointment/styles.scss';
 
 export default function Appointment(props) {
-  const booked = (time) => {
-    if (!time) {
-      return `No Appointments`
-    }
-    return `Appointment at ${time}`
-  }
+  // const booked = (time) => {
+  //   if (!time) {
+  //     return `No Appointments`
+  //   }
+  //   return `Appointment at ${time}`
+  // }
+  // {booked(props.time)}
+
 
   return (
-    <article className='appointment'>{booked(props.time)}</article>
+    <>
+
+      <article className='appointment'><Header time={props.time} />{props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty />} </article>
+    </>
   );
 }
