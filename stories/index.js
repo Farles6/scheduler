2199a6d49327ddbdb7ed771b6d8623f0c27380ff
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-20 14:20:10 -0400

    add props to status comp

--- stories/index.js
+++ stories/index.js
@@ -15,7 +15,7 @@ import Header from 'components/Appointment/Header';
 import Empty from 'components/Appointment/Empty';
 import Show from 'components/Appointment/Show';
 import Confirm from 'components/Appointment/Confirm';
-import Status from 'components/Application/Status';
+import Status from 'components/Appointment/Status';
 
 
 
