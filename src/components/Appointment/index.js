b6e0c49b0f1a254f235a1ba16f336ea1ad350c8e
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-22 19:08:46 -0400

    add useVisualMode to appointment comp

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,8 +2,13 @@ import React, { Fragment } from 'react';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import useVisualMode from 'hooks/useVisualMode';
+import Form from './Form';
 
 import 'components/Appointment/styles.scss';
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
 
 export default function Appointment(props) {
   // const booked = (time) => {
@@ -14,11 +19,20 @@ export default function Appointment(props) {
   // }
   // {booked(props.time)}
 
+  const { mode, transition, back } = useVisualMode(props.interview ? SHOW : EMPTY);
+
+
+
 
   return (
     <Fragment>
 
-      <article className='appointment'><Header time={props.time} />{props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer.name} /> : <Empty />} </article>
+      <article className='appointment'>
+        <Header time={props.time} />
+        {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+        {mode === CREATE && <Form  interviewers={[]} onSave={props.onSave} onCancel={() => back(props.onCancel)} />}
+        {mode === SHOW && (<Show student={props.interview.student} interviewer={props.interview.interviewer.name} />)}
+      </article>
     </Fragment>
   );
 }
--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -7,20 +7,18 @@ export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
   function transition(newMode, replace = false) {
-    console.log(replace);
     if (replace) {
-     history.pop()
+      history.pop();
     }
     setMode(newMode);
     setHistory(prev => [...prev, newMode]);
   }
+
   function back() {
     if (history.length > 1) {
       history.pop();
       setMode(history[history.length - 1]);
     }
   }
-  console.log(history);
-  console.log(mode);
   return { mode, transition, back };
 }
