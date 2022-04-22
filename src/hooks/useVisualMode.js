847c02dcc34febb799c5d5f83c759e0ecaded95f
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-22 18:29:49 -0400

    add replace para to transition

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,19 +4,23 @@ import { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  const [history , setHistory] = useState([initial])
-  
-  function transition(newMode) {
-    setMode(newMode)
-    setHistory(prev => [...prev, newMode])
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    console.log(replace);
+    if (replace) {
+     history.pop()
+    }
+    setMode(newMode);
+    setHistory(prev => [...prev, newMode]);
   }
   function back() {
-    if (history.length > 1){
-    history.pop()
-    setMode(history[history.length - 1])
+    if (history.length > 1) {
+      history.pop();
+      setMode(history[history.length - 1]);
     }
   }
-console.log(history);
-console.log(mode);
+  console.log(history);
+  console.log(mode);
   return { mode, transition, back };
 }
