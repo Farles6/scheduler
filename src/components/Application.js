228d7a3eba445e6bce0bbb1650ec620db12605b5
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-25 14:01:56 -0400

    add put request to save data to api

--- src/components/Application.js
+++ src/components/Application.js
@@ -25,7 +25,11 @@ export default function Application(props) {
       ...state.appointments,
       [id]: appointment
     };
-    setState(...state, appointments);
+
+    return axios.put(`/api/appointments/${id}`, appointment)
+      .then(() => {
+        setState({ ...state, appointments });
+      });
   }
 
 
