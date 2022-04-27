11c0bc50bb09d7e9131addb5be04dd94d1fe582b
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-27 13:57:39 -0400

    write cypress test to click on tuesday

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,10 +2,13 @@ describe('Navigation', () => {
   beforeEach(() => {
     cy.visit('/');
   });
-  it('should visit root', () => {
-    cy.visit('/');
-  });
-  it('should navigate to Tuesday', () => {
 
+  it('should visit root', () => {
+  });
+
+  it('should navigate to Tuesday', () => {
+    cy.contains('[data-testid=day]', 'Tuesday')
+      .click()
+      .should('have.class', 'day-list__item--selected');
   });
 });
