afe80ef952373dba61358f05c28d3b70f5e52f52
Tanner Farrelly <tannerfarrelly@gmail.com>
2022-04-27 13:21:14 -0400

    install cypress to app and write first test

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,11 @@
+describe('Navigation', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+  it('should visit root', () => {
+    cy.visit('/');
+  });
+  it('should navigate to Tuesday', () => {
+
+  });
+});
